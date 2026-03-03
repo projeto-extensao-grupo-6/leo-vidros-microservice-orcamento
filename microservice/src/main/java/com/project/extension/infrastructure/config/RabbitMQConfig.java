e179a5b43c938a2d38680da561eb9aa08278db8a
Phelipe <phelipe.silva@sptech.school>
2026-03-03 20:17:25 -0300

    Adicionado exchange e routing key no config da infra

--- microservice/src/main/java/com/project/extension/infrastructure/config/RabbitMQConfig.java
+++ microservice/src/main/java/com/project/extension/infrastructure/config/RabbitMQConfig.java
@@ -1,5 +1,8 @@
 package com.project.extension.infrastructure.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
@@ -8,18 +11,27 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
-
-    public static final String FILA_ORCAMENTO_PDF = "fila.orcamento.pdf";
+    public static final String QUEUE_NAME = "fila.orcamento.pdf";
+    public static final String EXCHANGE_NAME = "exchange.leovidros.direct";
+    public static final String ROUTING_KEY = "orcamento.gerar";
 
     @Bean
     public Queue orcamentoQueue() {
-        return new Queue(FILA_ORCAMENTO_PDF, true);
+        return new Queue(QUEUE_NAME, true);
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
     }
 
     @Bean
     public MessageConverter jsonMessageConverter() {
-        // O Spring Boot vai injetar o ObjectMapper aqui automaticamente
-        // assim que a dependência do Jackson estiver no pom.xml
         return new Jackson2JsonMessageConverter();
     }
 }
